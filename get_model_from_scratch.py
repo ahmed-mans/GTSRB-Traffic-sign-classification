7b046eca1e91c061c890fd5c050d855353439cef
ahmed-mans <55735381+ahmed-mans@users.noreply.github.com>
2025-03-01 20:02:41 +0100

    Create get_model_from_scratch.py

---
+++ get_model_from_scratch.py
@@ -0,0 +1,36 @@
+def get_model_from_scratch(num_classes, input_shape):
+
+    dp_value = 0.1
+    kernel_size = 7
+    inputs = Input(shape=input_shape)
+    x = Rescaling(1./255)(inputs)
+    
+    x = Conv2D(8, kernel_size, padding='valid')(x) 
+    x = Conv2D(16, kernel_size, padding='valid')(x)
+    x = Activation('relu')(x) 
+    x = MaxPooling2D()(x)    
+
+    x = Conv2D(32, kernel_size, padding='valid')(x)
+    x = Conv2D(64, kernel_size, padding='valid')(x)
+    x = Activation('relu')(x) 
+    x = MaxPooling2D()(x)   
+    
+    # Flatten the feature maps
+    flattened = Flatten()(x) 
+    
+    x = Dense(480, activation='relu')(flattened)           
+    x = Dropout(dp_value)(x)
+    x = Dense(240, activation='relu')(x)
+    x = Dropout(dp_value)(x)
+    x = Dense(120, activation='relu')(x)
+    x = Dropout(dp_value)(x)
+    x = Dense(84, activation='relu')(x)  
+    x = Dropout(dp_value)(x)
+    
+    # Softmax output layer.
+    headout = Dense(num_classes, activation="softmax")(x)  
+    
+    # Create model.
+    model = Model(inputs=inputs, outputs=headout, name='Model_1')
+    
+    return model
